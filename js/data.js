4dabb994ae292892afaf5bd1272c2775860e0240
tantirahma <tantirahma379@gmail.com>
2026-05-23 15:41:40 +0700

    update video link in subbab 2 and data.js

--- js/data.js
+++ js/data.js
@@ -286,7 +286,7 @@ function defaultContent() {
                   Amati video berikut dengan saksama. Perhatikan nyala api, asap, dan panas yang dihasilkan dari masing-masing bahan bakar!
                 </p>
                 <div class="videoWrap" style="margin-bottom:20px; border-radius:12px; overflow:hidden; box-shadow:0 4px 12px rgba(0,0,0,0.1);">
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/UZDto5boCJk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/mFls5tXQo9g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 </div>
               </div>
             </div>
