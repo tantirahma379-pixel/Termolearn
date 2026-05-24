1defd0aec3f25dcafff3418c2a24c0feaf7197ed
Agus Nurwanto <agusnurwantomuslim@gmail.com>
2026-05-24 14:46:59 +0700

    perbaikan error index.html

--- google_script.js
+++ google_script.js
@@ -372,6 +372,14 @@ function doGet(e) {
   html.email = e && e.parameter && e.parameter.email ? e.parameter.email : '';
   html.token = e && e.parameter && e.parameter.token ? e.parameter.token : '';
 
+  let gasUrl = '';
+  try {
+    gasUrl = ScriptApp.getService().getUrl();
+  } catch (err) {
+    // Fallback jika dijalankan langsung di editor Apps Script tanpa status Web App
+  }
+  html.gasUrl = gasUrl;
+
   return html.evaluate()
     .setTitle('ThermoLearn')
     .addMetaTag('viewport', 'width=device-width, initial-scale=1')
